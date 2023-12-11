6e7eea5409b60a8b31a9af40e56af014f862eea6
Khalil Masree <33801628+khalilmasri@users.noreply.github.com>
2023-12-11 21:46:17 +0200

    Added projects rendering/select at login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,38 @@
 using CodeBuggy.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using CodeBuggy.Data;
 
 namespace CodeBuggy.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
-
+        
         public IActionResult Index()
         {
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                List<Project> data = _context.Projects.ToList();
+                ViewBag.ProjectList = new SelectList(data, "Id", "Name");
+            }
+
             return View();
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            return View( );
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
