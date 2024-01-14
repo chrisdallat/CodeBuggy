05b7ba40a760bb396d748da8dd13e41129d66d0f
Chris Dallat <chr15dallat@hotmail.co.uk>
2024-01-14 12:43:10 +0000

    privacy page reinstated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,6 +26,11 @@ namespace CodeBuggy.Controllers
             return View( );
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
